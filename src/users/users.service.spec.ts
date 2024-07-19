f47538efd81e60bbe3b603f24d47630e0cbbb12e
Kim Seryung <eshika@naver.com>
2024-07-19 20:30:46 +0900

    8.1 mocking

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,22 @@
+import { Test } from '@nestjs/testing';
+import { UsersService } from './users.service';
+
+describe('users service', () => {
+  let service: UsersService;
+  beforeAll(async () => {
+    const testingModule = await Test.createTestingModule({
+      providers: [UsersService],
+    }).compile();
+    service = testingModule.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+});
