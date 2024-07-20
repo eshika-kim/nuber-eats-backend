bda52e4bdf444e15cd5f94f5c73f72f6749bb903
Kim Seryung <eshika@naver.com>
2024-07-20 18:52:35 +0900

    8.8 loginTestpart2까지 완료

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -16,7 +16,7 @@ const mockRepository = () => ({
 
 // 가짜 jwt service
 const mockJwtService = {
-  sign: jest.fn(),
+  sign: jest.fn(() => 'signed token'),
   verify: jest.fn(),
 };
 
@@ -25,10 +25,12 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UsersService', () => {
   let service: UsersService;
-
+  let jwtService: JwtService;
   let usersRepository: MockRepository<User>;
+  let verificationRepository: MockRepository<Verification>;
+
   // 테스트 모듈 주입
-  beforeAll(async () => {
+  beforeEach(async () => {
     const testModule = await Test.createTestingModule({
       providers: [
         UsersService,
@@ -47,7 +49,9 @@ describe('UsersService', () => {
       ],
     }).compile();
     service = testModule.get<UsersService>(UsersService);
+    jwtService = testModule.get<JwtService>(JwtService);
     usersRepository = testModule.get(getRepositoryToken(User));
+    verificationRepository = testModule.get(getRepositoryToken(Verification));
   });
 
   it('should be defined', () => {
@@ -71,14 +75,78 @@ describe('UsersService', () => {
         error: 'There is a user with that email already',
       });
     });
+
     it('should create a user', async () => {
       usersRepository.findOne.mockResolvedValue(undefined);
       usersRepository.create.mockReturnValue(createAccountArgs);
-      await service.createAccount(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      verificationRepository.create.mockReturnValue(createAccountArgs);
+      // 실행부
+      const result = await service.createAccount(createAccountArgs);
+
       expect(usersRepository.create).toHaveBeenCalledTimes(1);
       expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
       expect(usersRepository.save).toHaveBeenCalledTimes(1);
       expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verificationRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationRepository.save).toHaveBeenCalledWith(
+        createAccountArgs,
+      );
+
+      expect(result).toEqual({ ok: true });
+    });
+
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error('?'));
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toEqual({ ok: false, error: "Couldn't create" });
+    });
+  });
+
+  describe('login', () => {
+    const loginArgs = { email: 'abc@abc.com', password: 'bsps' };
+    it('should fail if user does not exist', async () => {
+      usersRepository.findOne.mockResolvedValue(null);
+      const result = await service.login(loginArgs);
+
+      expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(usersRepository.findOne).toHaveBeenCalledWith(expect.any(Object));
+      expect(result).toEqual({
+        ok: false,
+        error: 'User not found',
+      });
+    });
+
+    it('should fail if the password is wrong', async () => {
+      const mockedUser = {
+        id: 1,
+        checkPassword: jest.fn(() => Promise.resolve(false)),
+      };
+      usersRepository.findOne.mockResolvedValue(mockedUser);
+      const result = await service.login(loginArgs);
+      expect(result).toEqual({ ok: false, error: 'Wrong password!' });
+    });
+
+    it('should return token if password correct', async () => {
+      const mockedUser = {
+        id: 1,
+        checkPassword: jest.fn(() => Promise.resolve(true)),
+      };
+      usersRepository.findOne.mockResolvedValue(mockedUser);
+      const result = await service.login(loginArgs);
+      expect(jwtService.sign).toHaveBeenCalledTimes(1);
+      expect(jwtService.sign).toHaveBeenCalledWith(expect.any(Number));
+      expect(result).toEqual({
+        ok: true,
+        token: 'signed token',
+      });
     });
   });
 });
